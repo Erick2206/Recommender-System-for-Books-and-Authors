c8c23d174b4c98d16af6c70aeee55b1618712076
Aayush Gogia <aayushar@yahoo.com>
2017-05-29 14:37:11 +0530

    Documented Paper14.py

--- clean14.py
+++ clean14.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
+##Outfile name for cleaned data
 outfile="papers14edit.csv"
 data=pd.read_csv('papers14.csv')
 
-##print data.head()
+##Iterating contents of csv and removing unnecessary spaces
 for index,row in data.iterrows():
     row["keywords"]=row["keywords"].split('\n')
     if not pd.isnull(row["topics"]):
         row["topics"]=row["topics"].split("\n")
-#    print row["topics"]
     row["abstract"]=row["abstract"].replace('\n',' ')
     if not pd.isnull(row["groups"]):
         row['groups']=row['groups'].split("\n")
 
+##Giving new dataframe index a name
 data.index.name="index"
 data.to_csv(outfile, sep=',', encoding='utf-8')
