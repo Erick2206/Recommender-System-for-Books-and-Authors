812af8c4c10a3aa38042ab5d98f9a832c5260782
Aayush Gogia <aayushar@yahoo.com>
2017-05-30 12:56:54 +0530

    Completed dataBuild.py to make a csv file from dataset.

--- ReuterAnalysis/dataBuild.py
+++ ReuterAnalysis/dataBuild.py
@@ -1,12 +1,16 @@
 import os
 
 target="data.csv"
-writeMode="w"
+writeMode="wb"
 rootDir="/home/erick/Repo/InternWork/ReuterAnalysis/Sample"
 
 with open(target,writeMode) as out:
     for dirName, subDirList, fileList in os.walk(rootDir):
         print dirName
         for fName in fileList:
-            print fName
-        print len(fileList)
+            filePath=dirName+'/'+fName
+            with open(filePath,'rb') as inFile:
+                data=inFile.read().replace("\r\n","")
+                out.write(dirName.split('/')[-1]+',')
+                out.write(data)
+                out.write('\n')
