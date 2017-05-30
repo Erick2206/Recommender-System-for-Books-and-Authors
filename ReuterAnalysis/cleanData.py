d0d68ee87fce64ba409e2f6334a9b5234788dd99
Aayush Gogia <aayushar@yahoo.com>
2017-05-31 00:07:21 +0530

    Completed data preprocessing and some minor changes in dataBuild file

---
+++ ReuterAnalysis/cleanData.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+fileName="data.csv"
+cachedStopWords=stopwords.words("english")
+ps=PorterStemmer()
+
+data=pd.read_csv(fileName,delimiter="    ",engine="python")
+for index,row in data.iterrows():
+    word_dataset=[]
+    sentences=sent_tokenize(row['Article'])
+    ##words=[word for word in words if word not in cachedStopWords]
+    for words in sentences:
+        word=word_tokenize(words)
+        for allwords in word:
+            allwords=ps.stem(allwords)
+            word_dataset.append(allwords)
+    row['Article']=word_dataset
+
+data.index.name="Index"
+data.to_csv(fileName,sep='\t')
--- ReuterAnalysis/dataBuild.py
+++ ReuterAnalysis/dataBuild.py
@@ -5,12 +5,13 @@ writeMode="wb"
 rootDir="/home/erick/Repo/InternWork/ReuterAnalysis/Sample"
 
 with open(target,writeMode) as out:
+    out.write("Author    Article\n")
     for dirName, subDirList, fileList in os.walk(rootDir):
         print dirName
         for fName in fileList:
             filePath=dirName+'/'+fName
             with open(filePath,'rb') as inFile:
                 data=inFile.read().replace("\r\n","")
-                out.write(dirName.split('/')[-1]+',')
+                out.write(dirName.split('/')[-1]+'    ')
                 out.write(data)
                 out.write('\n')
