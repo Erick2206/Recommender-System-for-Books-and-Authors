60f60d40c6840819376bcb5bf44a0ebf4f332a69
Aayush Gogia <aayushar@yahoo.com>
2017-06-03 14:56:01 +0530

    Hyperparameter testing on 20 newsgroup

--- ReuterAnalysis/test.py
+++ ReuterAnalysis/test.py
@@ -1,17 +1,23 @@
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn import metrics
 import logging
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s %(message)s')
-dataset=fetch_20newsgroups(subset='all',shuffle=True,random_state=42)
+categories = ['alt.atheism',
+    'talk.religion.misc',
+    'comp.graphics',
+    'sci.space',
+]
+
+dataset=fetch_20newsgroups(subset='all',shuffle=True,random_state=42,categories=categories)
 labels=dataset.target
-vectorizer=CountVectorizer(max_df=0.5,min_df=2,stop_words='english',ngram_range=(2,2))
+vectorizer=TfidfVectorizer(max_df=1.0,min_df=10,stop_words='english',ngram_range=(1,2))
 
 X=vectorizer.fit_transform(dataset.data)
 
-km=KMeans(verbose=0,n_clusters=50,n_init=1)
+km=KMeans(verbose=0,n_clusters=50,n_init=5)
 km.fit(X)
 
 print "ARI: ", metrics.adjusted_rand_score(labels, km.labels_)
